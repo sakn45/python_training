e40e1877500a6cbcbb04ef0399b0ddf7b536dfff
as1ww <n45.suhanova@yandex.ru>
2022-07-20 20:49:54 +0300

    Интеллектуальная фикстура

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,15 +3,11 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="fhfujrbfj", header="rjbrjbrg", footer="rjfnrjgrj"))
-    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
 
 
 

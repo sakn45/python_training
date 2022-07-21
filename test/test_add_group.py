e156ecd63554f70b63bf18464677f9b9eb782462
as1ww <n45.suhanova@yandex.ru>
2022-07-21 09:43:58 +0300

    Повышаем "интеллектуальность" функций login и logout: проверка текущего состояния сессии(не до конца)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,6 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="fhfujrbfj", header="rjbrjbrg", footer="rjfnrjgrj"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):

5215c73556a6dca86536669f9c644fd30fff3ac9
as1ww <n45.suhanova@yandex.ru>
2022-07-21 09:43:58 +0300

    Повышаем "интеллектуальность" функций login и logout: проверка текущего состояния сессии(не до конца)

--- fixture/application.py
+++ fixture/application.py
@@ -6,8 +6,8 @@ from fixture.group import GroupHelper
 class Application:
 
     def __init__(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
 

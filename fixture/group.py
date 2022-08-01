7114642534cd453cc194d9c70d63335ee23e3afd
as1ww <n45.suhanova@yandex.ru>
2022-08-01 09:21:25 +0300

    Повышаем "интеллектуальность" тестов: обеспечение выполнения предусловий

--- fixture/group.py
+++ fixture/group.py
@@ -30,14 +30,11 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
-    def delete_first_group(self):
+    def fill_group_form(self, group):
         wd = self.app.wd
-        self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
-        # submit deletion
-        wd.find_element_by_name("delete").click()
-        self.return_to_groups_page()
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
@@ -45,17 +42,6 @@ class GroupHelper:
             wd.find_element_by_name(field_name).click()
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
-
-    def fill_group_form(self, group):
-        wd = self.app.wd
-        self.change_field_value("group_name", group.name)
-        self.change_field_value("group_header", group.header)
-        self.change_field_value("group_footer", group.footer)
-
-    def select_first_group(self):
-        wd = self.app.wd
-        # select first group
-        wd.find_element_by_name("selected[]").click()
 
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
@@ -69,3 +55,25 @@ class GroupHelper:
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def select_first_group(self):
+        wd = self.app.wd
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
+
+
+

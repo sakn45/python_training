fe67dfddcd7ad65bad813dbd6969a894886f97ca
as1ww <n45.suhanova@yandex.ru>
2022-08-16 21:43:25 +0300

    работает

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -17,6 +17,7 @@ def test_modify_group_name(app):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
+
 # def test_modify_group_header(app):
     # old_groups = app.group.get_group_list()
     # app.group.modify_first_group(Group(header="New header"))

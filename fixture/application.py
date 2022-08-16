a84fd2901be2bd9e50b270066c839719b287c82b
as1ww <n45.suhanova@yandex.ru>
2022-08-16 21:21:03 +0300

    работает

--- fixture/application.py
+++ fixture/application.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(3)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def is_valid(self):
         try:

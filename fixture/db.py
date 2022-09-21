27754c39db185e617c631278366e29bdf1cd4b44
as1ww <n45.suhanova@yandex.ru>
2022-09-21 18:50:11 +0300

    ORM – Object Relational Mapping

--- fixture/db.py
+++ fixture/db.py
@@ -5,7 +5,7 @@ from model.contact import Contact
 
 class DbFixture:
 
-    def __init__(self,host, name, user, password):
+    def __init__(self, host, name, user, password):
         self.host = host
         self.name = name
         self.user = user
